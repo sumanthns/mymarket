57336d3ba5021e10c22194f631339ea0572567e8
Sumanth <nsready@gmail.com>
2013-04-07 21:54:54 +0530

    Adding code base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mymarket::Application.config.secret_token = '36a2dd7f3c44a2874a50e06ff17ebc1f792d2f8bde7be836072b5d6342c3fdcea790d6e122a7b33aabebd6b1426a67cd44c1094017c0363fe6b59527c5a6e846'
